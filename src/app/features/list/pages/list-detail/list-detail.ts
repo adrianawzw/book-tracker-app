fbe4c1584e4ac08ca26a71cda486c8acce3ff388
Gerardo Ramos <jramos5191@gmail.com>
2026-06-25 21:33:14 -0500

    Visualizacion de la seccion lista de libros

---
+++ src/app/features/list/pages/list-detail/list-detail.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-list-detail',
+  imports: [],
+  templateUrl: './list-detail.html',
+  styleUrl: './list-detail.css',
+})
+export class ListDetail{
+
+}
