6d21b4bd9ba6b7b4ae2c6c1a42058684e30fa189
Gerardo Ramos <jramos5191@gmail.com>
2026-07-12 15:10:47 -0500

    correccion de la api constant

--- src/app/core/constants.ts
+++ src/app/core/constants.ts
@@ -1,2 +1,1 @@
-export const API_BASE_URL = 'http://localhost:8080';
-// 'https://book-tracker-api-c6e4ghewh5e3dkd5.canadacentral-01.azurewebsites.net/';
+export const API_BASE_URL = 'https://book-tracker-api-c6e4ghewh5e3dkd5.canadacentral-01.azurewebsites.net/';
